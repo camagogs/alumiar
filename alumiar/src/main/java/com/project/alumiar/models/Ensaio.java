1d8981cde7404efb35cc3001c7dba33a39eff8df
Braullio Soares <braulliosoares.bs@gmail.com>
2017-11-05 14:53:57 -0300

    Create model classes

---
+++ alumiar/src/main/java/com/project/alumiar/models/Ensaio.java
@@ -0,0 +1,57 @@
+package com.project.alumiar.models;
+
+public class Ensaio {
+	
+	private Long id;
+	
+	private String nomeDoEnsaio;
+	
+	private String nomDoFotografo;
+	
+	private String ong;
+	
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeDoEnsaio() {
+		return nomeDoEnsaio;
+	}
+
+	public void setNomeDoEnsaio(String nomeDoEnsaio) {
+		this.nomeDoEnsaio = nomeDoEnsaio;
+	}
+
+	public String getNomDoFotografo() {
+		return nomDoFotografo;
+	}
+
+	public void setNomDoFotografo(String nomDoFotografo) {
+		this.nomDoFotografo = nomDoFotografo;
+	}
+
+	public String getOng() {
+		return ong;
+	}
+
+	public void setOng(String ong) {
+		this.ong = ong;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+
+}
