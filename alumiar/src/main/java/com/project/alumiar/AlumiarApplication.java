97d8c23703b852a974bf6b36165a4960fdc94fda
Braullio Soares <braulliosoares.bs@gmail.com>
2017-11-05 13:16:56 -0300

    Create project for Spring Boot

---
+++ alumiar/src/main/java/com/project/alumiar/AlumiarApplication.java
@@ -0,0 +1,12 @@
+package com.project.alumiar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumiarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumiarApplication.class, args);
+	}
+}
