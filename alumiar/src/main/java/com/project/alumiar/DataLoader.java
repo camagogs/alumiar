69e3c0d18844c52928f90de0780047e13c89fd30
Braullio Soares <braulliosoares.bs@gmail.com>
2017-11-28 11:44:46 -0300

    [DEVBACK-00001] - Alumiar.com create form loading

--- alumiar/src/main/java/com/project/alumiar/DataLoader.java
+++ alumiar/src/main/java/com/project/alumiar/DataLoader.java
@@ -20,35 +20,35 @@ public class DataLoader {
 
 	@PostConstruct
 	private void createAnimal() {
-		Animal a = new Animal();
-		
-		a.setNome("Billy");
-		a.setTipoAnimal("Cachorro");
-		a.setRaca("Labrador");
-		a.setSexo("Macho");
-		a.setIsAdotado("Não Adotado");
-		a.setCuidados("Ração especial sem glúten");
-		a.setIdade("Filhote");
-		a.setDescricao("Billy é um cachorro dócil e brincalhão, gosta de passear e brincar com sua bola.");
-		a.setPelagem("Curta");
-		a.setPathFoto("dog.jpg");
-		
-		Animal g = new Animal();
-		
-		g.setNome("Belinha");
-		g.setTipoAnimal("Gato");
-		g.setRaca("Indefinido");
-		g.setSexo("Fêmea");
-		g.setIsAdotado("Não Adotado");
-		g.setCuidados("Ração especial sem glúten");
-		g.setIdade("Adulto");
-		g.setDescricao("Belinha é uma gata dócil.");
-		g.setPelagem("Longa");
-		g.setPathFoto("cat.png");
-		
-		repository.save(a);
-		
-		repository.save(g);
+//		Animal a = new Animal();
+//		
+//		a.setNome("Billy");
+//		a.setTipoAnimal("Cachorro");
+//		a.setRaca("Labrador");
+//		a.setSexo("Macho");
+//		a.setIsAdotado("Não Adotado");
+//		a.setCuidados("Ração especial sem glúten");
+//		a.setIdade("Filhote");
+//		a.setDescricao("Billy é um cachorro dócil e brincalhão, gosta de passear e brincar com sua bola.");
+//		a.setPelagem("Curta");
+//		a.setPathFoto("dog.jpg");
+//		
+//		Animal g = new Animal();
+//		
+//		g.setNome("Belinha");
+//		g.setTipoAnimal("Gato");
+//		g.setRaca("Indefinido");
+//		g.setSexo("Fêmea");
+//		g.setIsAdotado("Não Adotado");
+//		g.setCuidados("Ração especial sem glúten");
+//		g.setIdade("Adulto");
+//		g.setDescricao("Belinha é uma gata dócil.");
+//		g.setPelagem("Longa");
+//		g.setPathFoto("cat.png");
+//		
+//		repository.save(a);
+//		
+//		repository.save(g);
 		
 	}
 }
